fd72ee905d9e2666506229b667dcd8f492c5fc40
Sharan Raj <98011078+sharan-cd@users.noreply.github.com>
2022-08-10 19:12:23 +0530

    Update delete_access_key.py

--- delete_access_key.py
+++ delete_access_key.py
@@ -4,9 +4,9 @@ from botocore.exceptions import ClientError
 
 iam_client = boto3.client('iam')
 
-db_client = boto3.client('dynamodb')
+db_client = boto3.client('dynamodb', region_name = 'ap-south-1')
 
-db_resource = boto3.resource('dynamodb')
+db_resource = boto3.resource('dynamodb', region_name = 'ap-south-1')
 
 db_table = db_resource.Table('Users-List-IAM')
 
